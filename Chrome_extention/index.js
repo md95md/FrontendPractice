473269c52882966caf73ba3c3b32759f2329f43e
alinasett <kane1991kk@gmail.com>
2025-11-12 17:53:06 +0700

    first working version

--- Chrome_extention/index.js
+++ Chrome_extention/index.js
@@ -1,7 +1,25 @@
 
-let inputBtn = document.getElementById("input-btn")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked!")
+	myLeads.push(inputEl.value)
+	inputEl.value = ""
+	renderLeads()
 })
 
+function renderLeads() {
+	let listItems = ""
+	for (let i = 0; i < myLeads.length; i++) {
+		listItems += `
+		<li>
+			<a href=${myLeads[i]} target="_blank">
+				${myLeads[i]}
+			</a>
+		</li>
+		`
+	}
+	ulEl.innerHTML = listItems
+}
